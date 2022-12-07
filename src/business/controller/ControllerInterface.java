d2fe261afaa91cd3c8360df6be7128727b806ec5
nguyendangtu <nguyendangtu1987@gmail.com>
2022-12-07 13:55:39 -0600

    update checkout sequence diagram

--- src/business/controller/ControllerInterface.java
+++ src/business/controller/ControllerInterface.java
@@ -1,6 +1,7 @@
 package business.controller;
 
 import business.exception.LoginException;
+import business.model.CheckoutRecord;
 
 import java.util.List;
 
@@ -8,5 +9,21 @@ public interface ControllerInterface {
 	void login(String id, String password) throws LoginException;
 	List<String> allMemberIds();
 	List<String> allBookIds();
+
+	/***
+	 * Checkout book and return a checkout record which already checked out.
+	 * @param libraryMemberID
+	 * @param {@link String}
+	 * @return {@link CheckoutRecord} a checkout record
+	 */
+	CheckoutRecord checkoutBook(String libraryMemberID, String isbn);
+
+	/***
+	 * Checkout book and return a checkout record which already checked out.
+	 * @param libraryMemberID
+	 * @param listIsbn {@link List}
+	 * @return {@CheckoutRecord} a checkout record
+	 */
+	CheckoutRecord checkoutBook(String libraryMemberID, List<String> listIsbn);
 	
 }

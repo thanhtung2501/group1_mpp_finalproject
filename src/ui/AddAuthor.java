84c8421ce45ff5d91d84ba9cc3ef9b8581ac78ec
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2022-12-07 17:37:57 -0600

    Add form to add Author

---
+++ src/ui/AddAuthor.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+
+public class AddAuthor {
+    private JTextField txtFirstName;
+    private JTextField textField1;
+    private JTextField txtStreet;
+    private JTextField txtCity;
+    private JTextField txtState;
+    private JTextField txtZip;
+    private JTextField txtPhone;
+    private JCheckBox chbCredentials;
+    private JTextArea txaBio;
+    private JButton btnAdd;
+}

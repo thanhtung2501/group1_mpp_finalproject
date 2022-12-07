3624c96c168c5abf5357110e2f9ac3e11fe02d28
Thanh Dung Nguyen <dung.nguyenthanh@hotmail.com>
2022-12-07 14:17:55 -0600

    Add form to add new book

---
+++ src/ui/AddBook.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class AddBook {
+    private JTextField txtISBN;
+    private JTextField txtTitle;
+    private JComboBox cmbAuthors;
+    private JComboBox cmbMaxCheckoutLength;
+    private JComboBox cmbNumberOfCopies;
+    private JButton addButton;
+}
